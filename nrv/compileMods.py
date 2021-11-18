7a61ea6961756a4c91c8e95f608ca9c47407932a
louisreg <41673193+louisreg@users.noreply.github.com>
2021-11-18 17:56:10 +0100

    Add files via upload
    Adding CompileMods.py

---
+++ nrv/compileMods.py
@@ -0,0 +1,22 @@
+"""
+NRV-compileMods
+Authors: Florian Kolbl / Roland Giraud / Louis Regnacq
+(c) ETIS - University Cergy-Pontoise - CNRS
+"""
+
+import os
+
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+path2compiled_mods =  dir_path + '/mods/x86_64'
+
+def NeuronCompile():
+    #path2compiled_mods =  dir_path + '/mods/x86_64'
+    path2_mods = dir_path + '/mods'
+    os.system('cd ' +path2_mods+  '&& nrnivmodl')
+
+if (os.path.exists(path2compiled_mods)):
+    print('Mods files are compiled')
+else :
+    NeuronCompile()
+
