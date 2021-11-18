edf7aeb07a459f30a7a49d87e96690115fa8e225
louisreg <41673193+louisreg@users.noreply.github.com>
2021-11-18 16:40:49 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
    description='Neuron Virtualizer',
    author='Florian Kolbl - Roland Giraud - Louis Regnacq - Thomas Couppey',
    packages=['nrv'],  #same as name
-    data_files = [('nrv', glob('nested_source_dir/**/*', recursive=True))], # includes sub-folders - recursive
+    data_files = [('nrv', glob('source_dir/*', recursive=True))], # includes sub-folders - recursive
 
 
    #install_requires=['bar', 'greek'], #external packages as dependencies
