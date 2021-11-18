4eb53ebae09b2d469559851b38cfadc878866d2c
louisreg <41673193+louisreg@users.noreply.github.com>
2021-11-18 13:00:27 +0100

    Add files via upload
    Setup file to use with pip

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+   name='nrv',
+   version='0.0.1',
+   description='Neuron Virtualizer',
+   author='Florian Kolbl - Roland Giraud - Louis Regnacq - Thomas Couppey',
+   packages=['nrv'],  #same as name
+   #install_requires=['bar', 'greek'], #external packages as dependencies
+   #scripts=[
+#        'scripts/cool',
+#            'scripts/skype',
+#           ]
+)
