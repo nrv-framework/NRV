2d29f890c303792eaf22716e384f0c13f90d32ce
louisreg <41673193+louisreg@users.noreply.github.com>
2021-11-24 12:19:20 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,9 +10,9 @@ setup(
    include_package_data = True,
    url = 'https://github.com/fkolbl/NRV',
    classifiers =
-    Programming Language :: Python :: 3
-    License :: CeCILL
-    Operating System :: OS Independent
+    'Programming Language :: Python :: 3',
+    'License :: CeCILL',
+    'Operating System :: OS Independent',
     install_requires=['mph', 'numpy','scipy','faulthandler','math','matplotlib','pylab','numba','json','ezdxf','icecream'], #external packages as dependencies
     python_requires = >=3.6
 )
