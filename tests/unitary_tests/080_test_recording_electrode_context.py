1387b9c32359bd5d6d1d010d94592c3789837a29
fkolbl <43564140+fkolbl@users.noreply.github.com>
2022-02-01 15:03:08 +0100

    Update test to working commit of recording.py

--- tests/unitary_tests/080_test_recording_electrode_context.py
+++ tests/unitary_tests/080_test_recording_electrode_context.py
@@ -1,33 +1,47 @@
 import nrv
 import matplotlib.pyplot as plt
-
+## Axon def
 y = 0
 z = 0
 d = 1
 L = 5000
+model = "Rattay_Aberham" # Rattay_Aberham if not precised
 
+axon1 = nrv.unmyelinated(y, z, d, L, model=model)
+
+## test pulse
 t_start = 1
-duration = 0.5
-amplitude = 5
+duration = 0.1
+amplitude = 1
+axon1.insert_I_Clamp(0.01, t_start, duration, amplitude)
 
 # test recording
-testrec = nrv.recorder()
+testrec = nrv.recorder('endoneurium_bhadra')
 testrec.set_recording_point(L/4, 0, 100)
 testrec.set_recording_point(L/2, 0, 100)
 testrec.set_recording_point(3*L/4, 0, 100)
-print(len(testrec.recording_points))
-print(testrec.is_empty())
 
 
-# test 1: 1 section stim in middle
-axon1 = nrv.unmyelinated(y,z,d,L,dt=0.001,Nrec=100)
-axon1.insert_I_Clamp(0.01, t_start, duration, amplitude)
+
 axon1.attach_extracellular_recorder(testrec)
-results = axon1.simulate(t_sim=10)
+results = axon1.simulate(t_sim=20)
 
 plt.figure()
 for rec in testrec.recording_points:
-	print(rec.recording)
 	plt.plot(results['x_rec'], rec.footprints['0'])
+plt.savefig('./unitary_tests/figures/080_A.png')
 
-plt.show()
+plt.figure()
+for rec in testrec.recording_points:
+	plt.plot(results['t'],rec.recording)
+plt.savefig('./unitary_tests/figures/080_B.png')
+
+plt.figure()
+map = plt.pcolormesh(results['t'], results['x_rec'], results['V_mem'] ,shading='auto')
+plt.xlabel('time (ms)')
+plt.ylabel('position (µm)')
+cbar = plt.colorbar(map)
+cbar.set_label('membrane voltage')
+plt.savefig('./unitary_tests/figures/080_C.png')
+
+#plt.show()
