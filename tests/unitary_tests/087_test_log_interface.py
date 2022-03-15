efd02be8322561cc199c26b4398e6d5df230d2bf
ThomasCouppey <92871973+ThomasCouppey@users.noreply.github.com>
2022-03-15 14:18:10 +0100

    Delete 087_test_log_interface.py
    Removing test log interface double

--- tests/unitary_tests/087_test_log_interface.py
+++
@@ -1,8 +0,0 @@
-import nrv
-
-
-nrv.pass_info("This is a friendly message from NRV")
-
-nrv.rise_warning("This is a warning in yellow from NRV")
-
-nrv.rise_error('This is an error in red from NRV')
