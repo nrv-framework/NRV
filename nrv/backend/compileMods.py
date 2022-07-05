4c3a57306c8fa6941f366803a2fabc21658e168d
ThomasCouppey <92871973+ThomasCouppey@users.noreply.github.com>
2022-07-05 11:23:13 +0200

    Update compileMods.py

--- nrv/backend/compileMods.py
+++ nrv/backend/compileMods.py
@@ -13,6 +13,7 @@ path2compiled_mods =  dir_path + '/mods/x86_64'
 def NeuronCompile():
     #path2compiled_mods =  dir_path + '/mods/x86_64'
     path2_mods = dir_path + '/mods'
+    os.system('chmod +x '+ os.environ['NRVPATH'] +'/nrv2calm')
     os.system('cd ' +path2_mods+  '&& nrnivmodl')
 
 if not (os.path.exists(path2compiled_mods)):
