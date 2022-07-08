b99042b91daddb4b7e44306a7322cf5038e6a723
fkolbl <43564140+fkolbl@users.noreply.github.com>
2022-07-08 16:47:53 +0200

    functions to handle inputs/outpus
    first attempt to kind of unify object specification with flexible format

---
+++ nrv/backend/inputs.py
@@ -0,0 +1,9 @@
+from .log_interface import rise_error, rise_warning, pass_info
+
+def set_attributes(my_object, attributes_dict):
+    for key, value in attributes_dict.items():
+        if key in my_object.__dict__:
+            setattr(my_object, key, value)
+        else:
+            rise_warning('trying to set a non existing attribute '+str(key)+' in '+str(type(my_object)))
+    return 0
