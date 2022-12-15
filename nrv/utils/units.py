cd5424fe66120ec2a7cf60410f69d9bf17d5d445
ThomasCouppey <92871973+ThomasCouppey@users.noreply.github.com>
2022-12-15 21:28:16 +0100

    Update units.py
    add frequency and capacitance units

--- nrv/utils/units.py
+++ nrv/utils/units.py
@@ -1,6 +1,6 @@
 """
 NRV-Units
-Authors: Florian Kolbl / Roland Giraud / Louis Regnacq
+Authors: Florian Kolbl / Roland Giraud / Louis Regnacq / Thomas Couppey
 (c) ETIS - University Cergy-Pontoise - CNRS
 """
 
@@ -12,6 +12,8 @@ ms = 1			# default unit for time is ms, as neuron
 um = 1			# default unit for length is um, as neuron
 uA = 1			# default unit for current is uA, UNLESS NEURON who is by default in nA
 S = 1			# default unit for conductance is S
+kHz = 1         # default unit for frequency is kHz
+uF = 1          # default unit for capacitance is uF
 
 ######################
 ## voltage prefixes ##
@@ -27,6 +29,14 @@ s = sec = 1e3 * ms
 minute = 60 * sec
 hour = 60 * minute
 day = 24 * hour
+
+########################
+## frequency prefixes ##
+########################
+
+Hz = 1e-3 * kHz
+MHz = 1e3 * kHz
+GHz = 1e6 * kHz
 
 #####################
 ## length prefixes ##
@@ -49,3 +59,12 @@ A = 1e3 * mA
 kS = 1e3 * S
 mS = 1e-3 * S
 uS = 1e-6 * S
+
+
+###########################
+## capacitance prefixes ##
+###########################
+F = 1e6 * uF
+mF = 1e3 * uF
+pF = 1e-3 * uF
+nF = 1e-6 * uF
