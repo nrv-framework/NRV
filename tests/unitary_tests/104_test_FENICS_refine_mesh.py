cc0b8a73331209e7a930195bec4c898a96831d88
ThomasCouppey <92871973+ThomasCouppey@users.noreply.github.com>
2023-02-17 13:06:55 +0100

    Add files via upload

--- tests/unitary_tests/104_test_FENICS_refine_mesh.py
+++ tests/unitary_tests/104_test_FENICS_refine_mesh.py
@@ -31,7 +31,8 @@ mesh.add_domains(Out_face,22, dim=2, name=None)
 print(In_face, Out_face)
 
 feild_IDs = []
-feild_IDs += [mesh.refine_entities(ent_ID=In_face, res_in=0.2, dim=2, res_out=2, IncludeBoundary=True)]
+feild_IDs += [mesh.refine_entities(ent_ID=In_face, res_in=2, dim=2, res_out=2, IncludeBoundary=True)]
+
 feild_IDs += [mesh.refine_entities(ent_ID=Out_face, res_in=0.2, dim=2, res_out=2, IncludeBoundary=True)]
 feild_IDs += [mesh.refine_threshold(ent_ID=In_face, dim=2 ,res_min=0.2, dist_min=L/4, dist_max=3*L/4, res_max=2)]
 
@@ -39,7 +40,7 @@ print(feild_IDs)
 mesh.refine_min(feild_IDs=feild_IDs)
 
 mesh.save(mesh_file)
-mesh.save("./unitary_tests/sources/3cylinder")
+#mesh.save("./unitary_tests/sources/3cylinder")
 
 print(mesh.get_obj())
 #mesh.visualize()
