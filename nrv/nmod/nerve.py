bc8544ee3472c6a98bca6b3666eeef1fc8559eee
ThomasCouppey <92871973+ThomasCouppey@users.noreply.github.com>
2023-04-28 10:12:51 +0200

    Update and rename nerves.py to nerve.py
    start creating nerve class

--- nrv/nmod/nerve.py
+++ nrv/nmod/nerve.py
@@ -7,6 +7,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from ..backend.log_interface import rise_error, rise_warning, pass_info
 
+from .fascicles import *
+
 #################
 ## Nerve class ##
 #################
@@ -18,9 +20,8 @@ class nerve:
 
     a nerve can be instrumented by adding couples of electrodes+stimulus
     """
-    def __init__(self, outershape, simulation_box, materials,\
-            dt=0.001, Nseg_per_sec=0, freq=100, freq_min=0,\
-            mesh_shape='plateau_sigmoid', alpha_max=0.3, d_lambda=0.1, T=None, ID=0, threshold=-40,\
+    def __init__(self, Length=10000, Outer_D=5,  Nerve_D=4000, y_c=0, z_c=0,\
+            dt=0.001, mesh_shape='plateau_sigmoid', alpha_max=0.3, d_lambda=0.1, T=None, ID=0, threshold=-40,\
             Adelta_limit=1,**kwargs):
         """
         Instanciates an empty nerve.
@@ -46,14 +47,82 @@ class nerve:
         Adelta_limit    : float
             limit diameter between A-delta models (thin myelinated) and myelinated models for axons
         """
+        self.L = Length
+        self.y_c = y_c
+        self.z_c = z_c
+
+        self.Outer_D = (Outer_D*mm)
+
         self.L = L
         self.FEM = FEM
         ### empty attributes for later use
+        self.N_ax = 0
+        self.fascicles_IDs = []
         self.fascicles = []
+        self.electrodes_IDs = []
         self.electrodes = []
+        self.stimuli_IDs = []
         self.stimuli = []
         self.materials = {}
         self.extracellular_context = None
+
+    ## save/load methods
+    def save(self, fname, extracel_context=False, intracel_context=False, rec_context=False):
+        """
+        Save a nerve in a json file
+
+        Parameters
+        ----------
+        fname : str
+            name of the file to save the nerve
+        extracel_context: bool
+            if True, add the extracellular context to the saving
+        intracel_context: bool
+            if True, add the intracellular context to the saving
+        """
+        if MCH.do_master_only_work():
+            # copy everything into a dictionnary
+            nerve_config = {}
+            nerve_config['ID'] = self.ID
+
+            if intracel_context:
+                nerve_config['L'] = self.L
+
+            if extracel_context:
+                nerve_config['L'] = self.L
+
+            if rec_context:
+                nerve_config['record'] = self.record
+
+            # save the dictionnary as a json file
+            json_dump(nerve_config, fname)
+
+    def load(self, fname, extracel_context=False, intracel_context=False, rec_context=False):
+        """
+        Load a nerve configuration from a json file
+
+        Parameters
+        ----------
+        fname           : str
+            path to the json file describing a nerve
+        extracel_context: bool
+            if True, load the extracellular context as well
+        intracel_context: bool
+            if True, load the intracellular context as well
+        """
+        if type(fname) == str:
+            results = json_load(fname)
+        else: 
+            results = fname
+        self.ID = results['ID']
+
+        if intracel_context:
+            self.L = results['L']
+        if extracel_context:
+            self.L = results['L']
+            
+        if rec_context:
+            self.record = results['record']
 
     def add_fascicle(self, fascicle):
         """
@@ -64,7 +133,20 @@ class nerve:
         fascicle : object
             fascicle to add to the nerve struture
         """
-        raise NotImplementedError
+        if not is_fascicle(fascicle):
+            rise_warning('Only fascile object can be added with add fascicle method: nothing added')
+        else:
+            if fascicle.ID in self.fascicles_IDs:
+                pass_info('Fascicle ' +str(fascicle.ID)+' already in the nerve: will be replace')
+                i_fasc = np.index(fascicle.ID)
+                self.fascicles[i_fasc]
+            else:
+                self.fascicles_IDs += [fascicle.ID]
+                i_fasc = len(self.fascicles_IDs) + 1
+                self.fascicles += [fascicle]
+            self.fascicles[i_fasc].L = self.L
+            
+        
 
     def add_stimulation(self, electrode, stimulus):
         """
