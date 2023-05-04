5515789aeca1e5403592bc64a44c2f7b1c5b14c0
ThomasCouppey <92871973+ThomasCouppey@users.noreply.github.com>
2023-05-04 15:58:16 +0200

    Update myelinated.py
    simplify save/load

--- nrv/nmod/myelinated.py
+++ nrv/nmod/myelinated.py
@@ -441,7 +441,7 @@ class myelinated(axon):
         self.__get_seg_positions()
         self.__get_rec_positions()
 
-    def save_axon(self, save=False, fname='axon.json', extracel_context=False, intracel_context=False, rec_context=False):
+    def save(self, save=False, fname='axon.json', extracel_context=False, intracel_context=False, rec_context=False):
         """
         Return axon as dictionary and eventually save it as json file
 
@@ -457,7 +457,7 @@ class myelinated(axon):
         ax_dic : dict
             dictionary containing all information
         """
-        ax_dic = super().save_axon(extracel_context=extracel_context, intracel_context=intracel_context, rec_context=rec_context)
+        ax_dic = super().save(extracel_context=extracel_context, intracel_context=intracel_context, rec_context=rec_context)
 
         ax_dic['myelinated'] = self.myelinated
         ax_dic['thin'] = self.thin
@@ -469,7 +469,7 @@ class myelinated(axon):
             json_dump(ax_dic, fname)
         return ax_dic
 
-    def load_axon(self, data, extracel_context=False, intracel_context=False, rec_context=False):
+    def load(self, data, extracel_context=False, intracel_context=False, rec_context=False):
         """
         Load all axon properties from a dictionary or a json file
 
@@ -482,7 +482,7 @@ class myelinated(axon):
             ax_dic = json_load(data)
         else: 
             ax_dic = data
-        super().load_axon(data, extracel_context=extracel_context, intracel_context=intracel_context, rec_context=rec_context)
+        super().load(data, extracel_context=extracel_context, intracel_context=intracel_context, rec_context=rec_context)
 
         self.myelinated = ax_dic['myelinated']
         self.thin = ax_dic['thin']
@@ -504,7 +504,7 @@ class myelinated(axon):
             if ax_dic['intra_voltage_stim_stimulus'] is not None:
                 position = ax_dic['intra_voltage_stim_position'][0]
                 stim = stimulus()
-                stim.load_stimulus(ax_dic['intra_voltage_stim_stimulus'])
+                stim.load(ax_dic['intra_voltage_stim_stimulus'])
                 self.insert_V_Clamp(position/self.L, stim)
 
 
