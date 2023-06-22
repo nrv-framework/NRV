18444c5d07b4cf36421f3a93c533fcf2f73daca9
ThomasCouppey <92871973+ThomasCouppey@users.noreply.github.com>
2023-06-22 16:13:49 +0200

    Update compileMods.py
    Fixing bug for arm system

--- nrv/backend/compileMods.py
+++ nrv/backend/compileMods.py
@@ -1,22 +1,27 @@
 """
 NRV-compileMods
-Authors: Florian Kolbl / Roland Giraud / Louis Regnacq
+Authors: Florian Kolbl / Roland Giraud / Louis Regnacq / Thomas Couppey
 (c) ETIS - University Cergy-Pontoise - CNRS
 """
 
 import os
 
 
-dir_path = os.environ['NRVPATH'] + '/_misc'
-path2compiled_mods =  dir_path + '/mods/x86_64'
+dir_path = os.environ['NRVPATH'] + '/_misc/mods/'
+ls = os.listdir(dir_path)
+test_mods = False
+i=0
+while (not test_mods) and i<len(ls):
+    test_mods = not '.' in ls[i]
+    i += 1
 
 def NeuronCompile():
     #path2compiled_mods =  dir_path + '/mods/x86_64'
-    path2_mods = dir_path + '/mods'
+    path2_mods = dir_path
     os.system('chmod +x '+ os.environ['NRVPATH'] +'/nrv2calm')
     os.system('cd ' +path2_mods+  '&& nrnivmodl')
 
-if not (os.path.exists(path2compiled_mods)):
+if not test_mods:
     print('Mods files are not compiled, executing nrnivmodl...')
     NeuronCompile()
     print('Compilation done')
