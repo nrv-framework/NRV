64d705b79a9a7926045af3fa93a34bf1c43544e1
fkolbl <43564140+fkolbl@users.noreply.github.com>
2023-06-28 11:59:49 +0200

    Update Optimizers.py

--- nrv/optim/Optimizers.py
+++ nrv/optim/Optimizers.py
@@ -1,8 +1,19 @@
+from ..backend.log_interface import rise_error, rise_warning, pass_info
+from ..backend.file_handler import json_dump
+from ..backend.NRV_Class import NRV_class
+
+from abc import ABCMeta, abstractmethod
+
 import numpy as np
 import pyswarms
 
-class Optimizer(NRV_class):
-  def __init__(self, ndim):
-    self._method = None
-    self._ndim = ndim
 
+class Optimizer(NRV_class, metaclass=ABCMeta):
+  @abstractmethod
+  def __init__(self, method=None):
+    self._method = method
+
+class PSO_optimizer(Optimizer):
+  def __init__(self):
+    super().__init__('PSO')
+    pass
