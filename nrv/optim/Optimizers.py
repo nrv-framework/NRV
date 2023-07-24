57eb8075f27242a3bea92d221689cf9e04803594
ThomasCouppey <92871973+ThomasCouppey@users.noreply.github.com>
2023-07-24 15:38:17 +0200

    Update Optimizers.py
    pyswarms commented (prevent log issue)

--- nrv/optim/Optimizers.py
+++ nrv/optim/Optimizers.py
@@ -5,7 +5,7 @@ from ..backend.NRV_Class import NRV_class
 from abc import ABCMeta, abstractmethod
 
 import numpy as np
-import pyswarms
+#import pyswarms
 
 
 class Optimizer(NRV_class, metaclass=ABCMeta):
