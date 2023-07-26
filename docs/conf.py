3e84549de867389439592b39bbae79594a9a1fd0
fkolbl <43564140+fkolbl@users.noreply.github.com>
2023-07-26 10:34:58 +0200

    Add files via upload

--- docs/conf.py
+++ docs/conf.py
@@ -1,8 +1,21 @@
 # Configuration file for the Sphinx documentation builder.
 import os
 import sys
+from unittest.mock import MagicMock    # mock imports
 
 sys.path.insert(0, os.path.abspath('..'))
+
+# prevent from unnistalled requierements for nrv
+deps = ('mph', 'neuron', 'icecream', 'numba', 'mpi4py', 'scipy', 
+        'numpy', 'ezdxf', 'dolfinx', 'petsc4py', 'ufl', 'gmsh', 'dolfinx.io', 
+        'petsc4py.PETSc', 'dolfinx.fem', 'dolfinx.fem.petsc', 'dolfinx.io.utils',
+        'scipy.interpolate', 'scipy.special', 'dolfinx.io.gmshio', 'dolfinx.geometry',
+        'dolfinx.mesh', 'numpy.linalg',  'matplotlib', 'matplotlib.pyplot', 'numpy.core', 
+        'numpy.core._multiarray_umath', 'matplotlib._path', 'scipy.stats', 'scipy.optimize',
+        'matplotlib.pylab', 'pylab', 'scipy.spatial', 'scipy.sparse', 'scipy.sparse.csgraph')
+for package in deps:
+    sys.modules[package] = MagicMock()
+
 
 
 # -- Project information -----------------------------------------------------
