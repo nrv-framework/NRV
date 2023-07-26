d4a385e1614c3ba49337ba6405a04e50290d8822
fkolbl <43564140+fkolbl@users.noreply.github.com>
2023-07-26 10:35:29 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,12 +1,27 @@
 from setuptools import setup
+from unittest.mock import MagicMock    # mock imports
+import sys
+
+# prevent from unnistalled requierements for nrv
+deps = ('mph', 'neuron', 'icecream', 'numba', 'mpi4py', 'scipy', 
+        'numpy', 'ezdxf', 'dolfinx', 'petsc4py', 'ufl', 'gmsh', 'dolfinx.io', 
+        'petsc4py.PETSc', 'dolfinx.fem', 'dolfinx.fem.petsc', 'dolfinx.io.utils',
+        'scipy.interpolate', 'scipy.special', 'dolfinx.io.gmshio', 'dolfinx.geometry',
+        'dolfinx.mesh', 'numpy.linalg',  'matplotlib', 'matplotlib.pyplot', 'numpy.core', 
+        'numpy.core._multiarray_umath', 'matplotlib._path', 'scipy.stats', 'scipy.optimize',
+        'matplotlib.pylab', 'pylab', 'scipy.spatial', 'scipy.sparse', 'scipy.sparse.csgraph')
+for package in deps:
+    sys.modules[package] = MagicMock()
+
+import nrv
 
 setup(
    name='nrv-py',
-   version='0.0.2',
-   description='NeuRon Virtualizer',
+   version=nrv.__version__,
+   description=nrv.__project__,
    long_description = 'file: README.md',
-   author='Florian Kolbl - Roland Giraud - Louis Regnacq - Thomas Couppey',
-   packages=['nrv'],  #same as name
+   author=nrv.__contributors__,
+   packages=['nrv'],
    include_package_data = True,
    url = 'https://github.com/fkolbl/NRV',
    classifiers =[
