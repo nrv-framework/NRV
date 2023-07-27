554c4c004861b5b0ab72da602c12f577df6d4b59
fkolbl <43564140+fkolbl@users.noreply.github.com>
2023-07-27 13:22:47 +0200

    handling environnement variables
    added few lines to handle correctly the

--- nrv/__init__.py
+++ nrv/__init__.py
@@ -10,6 +10,37 @@ __contributors__    = 'Florian Kolbl, Roland Giraud, Louis Regnacq, Thomas Coupp
 __copyright__       = '2023, Florian Kolbl'
 __license__         = 'CeCILL'
 
+#################################
+# check environnement variables #
+#################################
+import os
+import inspect
+import platform
+# create a dummy object to locate frameworks path
+class DummyClass: pass
+nrv_path = os.path.dirname(os.path.abspath(inspect.getsourcefile(DummyClass)))
+root_path = nrv_path.replace('/nrv/','')
+# check the PATH with os.environ['PATH'], modify bash/zsh profile
+current_PATH = os.environ['PATH']
+conf_file = '.bashrc'
+if platform.system() == 'Darwin':
+    conf_file = '.zshrc'
+if not (nrv_path+':' in current_PATH or ':'+nrv_path in current_PATH):
+    with open(os.path.expanduser('~/'+conf_file), "a") as outfile:
+        outfile.write('\n\n\n# >>>>> NRV setup >>>>>\n')
+        outfile.write('export PATH="'+nrv_path+':$PATH"\n')
+        outfile.write('# <<<<< NRV setup <<<<<\n')
+    outfile.close()
+    print(conf_file + ' file modified, please source or restart console to be able to used nrv2calm')
+# create the environnement variable NRVPATH if it does not exist
+if not 'NRVPATH' in os.environ:
+    os.environ['NRVPATH'] = nrv_path
+# change the permissions on nrv2calm
+if not os.access(nrv_path+'/nrv2calm', os.X_OK):
+    mode = os.stat(nrv_path+'/nrv2calm').st_mode
+    mode |= (mode & 0o444) >> 2    # copy R bits to X
+    os.chmod(nrv_path+'/nrv2calm', mode)
+
 # Public interface
 from .backend import compileMods
 from .backend.parameters import *
