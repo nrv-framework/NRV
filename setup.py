9552ea28eed4ef5d246a8a61648ef5f17c4bfd01
Florian Kolbl <florian.kolbl@cyu.fr>
2023-09-27 14:07:01 +0200

    New action for pipy release by tagging master branch, superlinter renewed for future dev branch

--- setup.py
+++ setup.py
@@ -1,64 +1,132 @@
-from setuptools import setup, find_packages
-from unittest.mock import MagicMock    # mock imports
+from setuptools import setup
+from unittest.mock import MagicMock  # mock imports
 import sys
+import codecs
+import os
 
-# prevent from unnistalled requierements for nrv
-deps = ('mph', 'neuron', 'icecream', 'numba', 'mpi4py', 'scipy', 
-        'numpy', 'ezdxf', 'dolfinx', 'petsc4py', 'ufl', 'gmsh', 'dolfinx.io', 
-        'petsc4py.PETSc', 'dolfinx.fem', 'dolfinx.fem.petsc', 'dolfinx.io.utils',
-        'scipy.interpolate', 'scipy.special', 'dolfinx.io.gmshio', 'dolfinx.geometry',
-        'dolfinx.mesh', 'numpy.linalg',  'matplotlib', 'matplotlib.pyplot', 'numpy.core', 
-        'numpy.core._multiarray_umath', 'matplotlib._path', 'scipy.stats', 'scipy.optimize',
-        'matplotlib.pylab', 'pylab', 'scipy.spatial', 'scipy.sparse', 'scipy.sparse.csgraph')
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+# This gets deployed when a new release is made by github actions
+VERSION = '{{VERSION_PLACEHOLDER}}'
+
+# CHANGEME VARS
+PACKAGE_NAME = 'nrv-py'
+DESCRIPTION = 'NeuRon Virtualizer (NRV)'
+AUTHOR_NAME = 'Florian Kolbl, Roland Giraud, Louis Regnacq, Thomas Couppey'
+PROJECT_URL = "https://github.com/fkolbl/NRV"
+
+
+# prevent from unnistalled requierements for nrv import
+deps = (
+    "mph",
+    "neuron",
+    "icecream",
+    "numba",
+    "mpi4py",
+    "scipy",
+    "numpy",
+    "ezdxf",
+    "dolfinx",
+    "petsc4py",
+    "ufl",
+    "gmsh",
+    "dolfinx.io",
+    "petsc4py.PETSc",
+    "dolfinx.fem",
+    "dolfinx.fem.petsc",
+    "dolfinx.io.utils",
+    "scipy.interpolate",
+    "scipy.special",
+    "dolfinx.io.gmshio",
+    "dolfinx.geometry",
+    "dolfinx.mesh",
+    "numpy.linalg",
+    "matplotlib",
+    "matplotlib.pyplot",
+    "numpy.core",
+    "numpy.core._multiarray_umath",
+    "matplotlib._path",
+    "scipy.stats",
+    "scipy.optimize",
+    "matplotlib.pylab",
+    "pylab",
+    "scipy.spatial",
+    "scipy.sparse",
+    "scipy.sparse.csgraph",
+)
 for package in deps:
     sys.modules[package] = MagicMock()
 
 import nrv
 
 setup(
-   name='nrv-py',
-   version=nrv.__version__,
-   description=nrv.__project__,
-   long_description = 'file: README.md',
-   author=nrv.__contributors__,
-   packages=['nrv',
-             'nrv._misc',
-             'nrv._misc.OTF_PP',
-             'nrv._misc.comsol_templates',
-             'nrv._misc.geom',
-             'nrv._misc.log',
-             'nrv._misc.materials',
-             'nrv._misc.mods',
-             'nrv._misc.pops',
-             'nrv._misc.ppops',
-             'nrv._misc.stats',
-             'nrv.backend',
-             'nrv.fmod',
-             'nrv.fmod.FEM',
-             'nrv.fmod.FEM.fenics_utils',
-             'nrv.fmod.FEM.mesh_creator',
-             'nrv.nmod',
-             'nrv.optim',
-             'nrv.utils',
-             'nrv.utils.cell',
-             'nrv.utils.fascicle'
-             ],
-   package_data={'nrv':['nrv2calm'],
-                 'nrv._misc':['NRV.ini'],
-                 'nrv._misc.comsol_templates':['*.mph'],
-                 'nrv._misc.geom':['*.dxf', '*.png'],
-                 'nrv._misc.log':['NRV.log'],
-                 'nrv._misc.materials':['*.mat'],
-                 'nrv._misc.mods':['*.mod'],
-                 'nrv._misc.pops':['*.pop'],
-                 'nrv._misc.ppops':['*.ppop'],
-                 'nrv._misc.stats':['*.csv']},
-   include_package_data = True,
-   url = 'https://github.com/fkolbl/NRV',
-   classifiers =[
-    'Programming Language :: Python :: 3',
-    'Operating System :: OS Independent'
-     ],
-    install_requires=['mph', 'numpy','scipy','neuron','matplotlib','numba','ezdxf','icecream'], #external packages as dependencies
-    python_requires = '>=3.6'
+    # meta infos
+    name=PACKAGE_NAME,
+    version=VERSION,
+    author=AUTHOR_NAME,
+    description=DESCRIPTION,
+    url=PROJECT_URL,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    long_description="file: README.md",
+    
+    # architecture
+    packages=[
+        "nrv",
+        "nrv._misc",
+        "nrv._misc.OTF_PP",
+        "nrv._misc.comsol_templates",
+        "nrv._misc.geom",
+        "nrv._misc.log",
+        "nrv._misc.materials",
+        "nrv._misc.mods",
+        "nrv._misc.pops",
+        "nrv._misc.ppops",
+        "nrv._misc.stats",
+        "nrv.backend",
+        "nrv.fmod",
+        "nrv.fmod.FEM",
+        "nrv.fmod.FEM.fenics_utils",
+        "nrv.fmod.FEM.mesh_creator",
+        "nrv.nmod",
+        "nrv.optim",
+        "nrv.utils",
+        "nrv.utils.cell",
+        "nrv.utils.fascicle",
+    ],
+    
+    # non python data to keep
+    package_data={
+        "nrv": ["nrv2calm"],
+        "nrv._misc": ["NRV.ini"],
+        "nrv._misc.comsol_templates": ["*.mph"],
+        "nrv._misc.geom": ["*.dxf", "*.png"],
+        "nrv._misc.log": ["NRV.log"],
+        "nrv._misc.materials": ["*.mat"],
+        "nrv._misc.mods": ["*.mod"],
+        "nrv._misc.pops": ["*.pop"],
+        "nrv._misc.ppops": ["*.ppop"],
+        "nrv._misc.stats": ["*.csv"],
+    },
+    include_package_data=True,
+    # classifiers
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    # dependencies
+    install_requires=[
+        "mph",
+        "numpy",
+        "scipy",
+        "neuron",
+        "matplotlib",
+        "numba",
+        "ezdxf",
+        "icecream",
+    ],  # external packages as dependencies
+    python_requires=">=3.9",
 )
