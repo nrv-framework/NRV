fcbd78aa73fcc3428b3303e0eab488bc95ee8a04
Florian Kolbl <43564140+fkolbl@users.noreply.github.com>
2023-12-12 16:12:59 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
     author=AUTHOR_NAME,
     description=DESCRIPTION,
     url=PROJECT_URL,
-    version="0.9.16",
+    version="0.9.17",
     long_description=long_description,
     long_description_content_type='text/markdown',
 
