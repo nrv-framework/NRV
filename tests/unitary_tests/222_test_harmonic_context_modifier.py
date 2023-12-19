8d4867c25135f2eb105ff30d7183df6a1dd6ddb7
louisr <l.regnacq@gmail.com>
2023-12-19 18:11:47 +0100

    add harmonic pulse

--- tests/unitary_tests/222_test_harmonic_context_modifier.py
+++ tests/unitary_tests/222_test_harmonic_context_modifier.py
@@ -2,7 +2,8 @@ import nrv
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot_stim(stim, t_stop=None, N_plot=1000):
+def plot_stim(stim, t_stop=None, N_plot=10000):
+    '''
     stim = nrv.load_any(stim)
     if t_stop is None:
         t_stop = stim.t[-1]
@@ -11,18 +12,24 @@ def plot_stim(stim, t_stop=None, N_plot=1000):
     stim2.t = np.linspace(0, t_stop, N_plot)
     stim2 += stim
     plt.plot(stim2.t, stim2.s)
-test_stim_CM = nrv.biphasic_stimulus_CM(start="0", I_cathod="1")
+    '''
+    plt.step(stim.t, stim.s,where='post')
+
+start = 1
+t_pulse = 100e-3
+amplitude = 200
+
+test_stim_CM = nrv.harmonic_stimulus_CM(start = start,t_pulse=t_pulse)
+
 
 t_sim=5
 static_context = "./unitary_tests/sources/202_axon.json"
-X = np.array([
-    [1, 1],
-    [1, 20],
-    [1, 40],
-    [1, 60],
-    [1, 80],
-    [1, 100],
-])
+X = [
+    [amplitude,0.2,0],
+    [amplitude,0.2,0,0.4,0],
+    [amplitude,0.2,0,0.4,0,0.6,0],
+    [amplitude,0.8,0,0.4,0,0.6,0,0.8,0]
+]
 
 results = []
 # simulate the axon
@@ -31,8 +38,15 @@ plt.figure(2)
 for i, x in enumerate(X):
     print(x)
     ax = test_stim_CM(x, static_context)
+    test_stim = ax.extra_stim.stimuli[0]
+    '''
+    plt.plot(test_stim.t,test_stim.s)
+    plt.show()
+    exit()
+    '''
     results = ax(t_sim=t_sim)
     del ax
+
 
     plt.figure(1)
     results.plot_stim(0, t_stop=t_sim)
@@ -46,8 +60,8 @@ for i, x in enumerate(X):
 cbar = plt.colorbar(map)
 cbar.set_label('membrane voltage (mV)')
 plt.figure(1)
-plt.savefig('./unitary_tests/figures/202_A.png')
+plt.savefig('./unitary_tests/figures/222_A.png')
 plt.figure(2)
-plt.savefig('./unitary_tests/figures/202_B.png')
+plt.savefig('./unitary_tests/figures/222_B.png')
 
-#plt.show()
+plt.show()
