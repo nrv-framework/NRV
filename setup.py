8ee3d2d5aa7cdd2a357df40071b607e41d57d9fc
Florian Kolbl <florian.kolbl@cyu.fr>
2024-01-12 15:08:34 +0100

    Bump version: 0.9.29 → 0.9.30

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
     author=AUTHOR_NAME,
     description=DESCRIPTION,
     url=PROJECT_URL,
-    version="0.9.29",
+    version="0.9.30",
     long_description=long_description,
     long_description_content_type='text/markdown',
     # architecture
