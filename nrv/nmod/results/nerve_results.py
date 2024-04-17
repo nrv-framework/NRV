6a8e82ef2979c110a13bfa16dfe9e55c44b5426f
louisr <l.regnacq@gmail.com>
2024-04-17 11:55:36 +0200

    add test nerve results

--- nrv/nmod/results/nerve_results.py
+++ nrv/nmod/results/nerve_results.py
@@ -4,6 +4,8 @@ NRV-:class:`.nerve_results` handling.
 
 
 from ...backend.NRV_Results import sim_results
+from .fascicles_results import fascicle_results
+from ...backend.log_interface import rise_error, rise_warning, pass_info
 
 class nerve_results(sim_results):
     """
@@ -12,6 +14,13 @@ class nerve_results(sim_results):
 
     def __init__(self, context=None):
         super().__init__(context)
+
+
+    def get_fascicle_results(self, ID: int) -> fascicle_results:
+        if ID not in self.fascicles_IDs:
+            rise_error(("Fascicle ID does not exists."))
+        else:
+            return(self[f'fascicle{ID}'])
 
 
 
---
+++ tests/unitary_tests/518_test_nerves_results.py
@@ -0,0 +1,3 @@
+import nrv
+import numpy as np
+import matplotlib.pyplot as plt
