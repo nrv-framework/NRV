310588a424ee300002ae8a9c6a90f0ad510ca08c
Thomas Couppey <thomas.couppey.s@gmail.com>
2024-11-08 17:56:55 +0100

    fixing test issues

--- tests/unitary_tests/310_test_nerve_mesh.py
+++ tests/unitary_tests/310_test_nerve_mesh.py
@@ -3,20 +3,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-
-test_num = 310
-mesh_file_u = f"./unitary_tests/results/mesh/{test_num}_umesh"
-mesh_file_m = f"./unitary_tests/results/mesh/{test_num}_mmesh"
-
-
-fascicle = nrv.fascicle(ID=test_num)
-fascicle.axons_diameter = np.asarray([10 ,3, 4])
-fascicle.axons_type = np.asarray([1, 1, 1])
-fascicle.axons_y = np.asarray([0, 0, 10])
-fascicle.axons_z = np.asarray([0, 10, 0])
-fascicle.define_circular_contour(D=50)
-
 if nrv.MCH.do_master_only_work():
+
+    test_num = 310
+    mesh_file_u = f"./unitary_tests/results/mesh/{test_num}_umesh"
+    mesh_file_m = f"./unitary_tests/results/mesh/{test_num}_mmesh"
+
+
+    fascicle = nrv.fascicle(ID=test_num)
+    fascicle.axons_diameter = np.asarray([10 ,3, 4])
+    fascicle.axons_type = np.asarray([1, 1, 1])
+    fascicle.axons_y = np.asarray([0, 0, 10])
+    fascicle.axons_z = np.asarray([0, 10, 0])
+    fascicle.define_circular_contour(D=50)
+
     fig, ax = plt.subplots(figsize=(5,5))
     fascicle.plot(ax)
     #plt.show()
@@ -25,17 +25,15 @@ if nrv.MCH.do_master_only_work():
     print(mesh.n_core)
     mesh.compute_mesh()
     mesh.save(mesh_file_m)
-    plt.show()
-exit()
+    exit()
 
-fascicle = nrv.fascicle(ID=test_num)
-fascicle.axons_diameter = np.asarray([1, 1, 2])
-fascicle.axons_type = np.asarray([0, 0, 0])
-fascicle.axons_y = np.asarray([0, 0, 5])
-fascicle.axons_z = np.asarray([0, 2, 0])
-fascicle.define_circular_contour(D=50)
+    fascicle = nrv.fascicle(ID=test_num)
+    fascicle.axons_diameter = np.asarray([1, 1, 2])
+    fascicle.axons_type = np.asarray([0, 0, 0])
+    fascicle.axons_y = np.asarray([0, 0, 5])
+    fascicle.axons_z = np.asarray([0, 2, 0])
+    fascicle.define_circular_contour(D=50)
 
-if nrv.MCH.do_master_only_work():
 
     fig, ax = plt.subplots(figsize=(5,5))
     fascicle.plot(ax)
