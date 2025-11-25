Problem fixes: harmonic eit / rotation bug for n_elec not divisible by 4 Addigs Plot EIT 12-14 elec dv

nrv/eit/_eit2d.py
@@ -81,6 +81,7 @@ class EIT2DProblem(eit_forward):
     ):
         self.sigma_method: str = "avg_ind"
         super().__init__(nervefile, res_dname=res_dname, label=label, **parameters)
+        self.use_gnd_elec = True
 
     @property
     def dim(self) -> int:
@@ -247,14 +248,15 @@ class EIT2DProblem(eit_forward):
             bar = gmsh.model.occ.addRectangle(
                 0, -self.w_elec / 2, 0, self.r_cir * 1.1, self.w_elec
             )
-            angle = (np.pi / 2) - ((2 * np.pi * i) / (self.n_elec))
+            # TC 25/11/13: np.pi align first electrode with nerve circle construction point.
+            angle = np.pi - ((2 * np.pi * i) / (self.n_elec))
             gmsh.model.occ.rotate([(2, bar)], 0, 0, 0, 0, 0, 1, angle)
             bar_ids += [(2, bar)]
 
             # Compute the electrode center of map to ecover the line id
             z_elec_com = CoF_arc * np.exp(1j * angle)
-            elec_coms[i][0] = np.real(z_elec_com)
-            elec_coms[i][1] = np.imag(z_elec_com)
+            elec_coms[i][1] = -np.real(z_elec_com)
+            elec_coms[i][0] = np.imag(z_elec_com)
 
         dis3 = gmsh.model.occ.addDisk(
             0, 0, 0, self.r_cir, self.r_cir, zAxis=_zaxis, xAxis=_xaxis
@@ -277,7 +279,6 @@ class EIT2DProblem(eit_forward):
             for i in range(self.n_elec):
                 if np.allclose([x, y], elec_coms[i]):
                     line_elec[i] += [line[1]]
-
         surfs = gmsh.model.occ.getEntities(dim=2)
         id_elt_ne = []
         id_elt_fa = [[] for _ in range(self.n_fa)]

nrv/eit/results/_eit_forward_results.py
@@ -374,9 +374,7 @@ class eit_forward_results(dict):
             phi = self["v_eit_phase"][
                 self.ix_(i_t=i_t, i_e=i_e, i_f=i_f, i_p=i_p, **kwgs)
             ].squeeze()
-            sign = 2 * (phi < np.pi) - 1
             _v = _v * np.cos(phi)
-            # _v = _v * sign
 
         if __filter_res and self.has_failed_test:
             _v = np.delete(_v, self.fail_results, axis=0)
@@ -950,7 +948,7 @@ class eit_forward_results(dict):
             if not self.is_multi_freqs:
                 print("WARNING: cannot plot quasistatic results over frequencies")
                 return None
-            elif i_f is not None:
+            elif i_f is None:
                 _x = self["f"]
             else:
                 _x = self["f"][i_f]

nrv/eit/utils/_eit_plot.py
@@ -34,14 +34,13 @@ class Figure_elec:
     def __init_figure(self, **fig_kwgs):
         if self.n_e == 8:
             fig_dim = (3, 3)
-            i_plots = np.array(
-                [[0, 1], [0, 2], [1, 2], [2, 2], [2, 1], [2, 0], [1, 0], [0, 0]]
-            )
+            i_plots = [
+                [0, 1], [0, 2], [1, 2], [2, 2], [2, 1], [2, 0], [1, 0], [0, 0]
+                 ]
             i_center = (1, 1)
         elif self.n_e == 12:
             fig_dim = (4, 4)
-            i_plots = np.array(
-                [
+            i_plots = [
                     [0, 1],
                     [0, 2],
                     [0, 3],
@@ -53,14 +52,12 @@ class Figure_elec:
                     [3, 0],
                     [1, 0],
                     [0, 0],
-                ]
-            )
+            ]
             idxs = np.arange(2) + 1
             i_center = (slice(1, 3), slice(1, 3))
         elif self.n_e == 16:
             fig_dim = (5, 5)
-            i_plots = np.array(
-                [
+            i_plots = [
                     [0, 2],
                     [0, 3],
                     [0, 4],
@@ -77,8 +74,7 @@ class Figure_elec:
                     [1, 0],
                     [0, 0],
                     [0, 1],
-                ]
-            )
+            ]
             i_center = (slice(1, 4), slice(1, 4))
 
         mask = np.ones(fig_dim, dtype=bool)